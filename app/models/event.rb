c5ec825aed1cf14f7b427504150406bec6b774d9
Jordan <jordanmcrae@hotmail.com>
2015-05-20 15:30:57 -0400

    Started writing basic tests for Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,5 @@
 class Event < ActiveRecord::Base
   validates :event_name, :address_1, :city, :country, :number_of_attendees, :time, :description, :category, presence: true
-  validates :event_name, exclusion: { in: %w(www us ca jp), #The exclusion helper has an option :in that receives the set of values that will not be accepted for the validated attributes.
-    message: "%{value} is reserved." }
   validates :event_name, length: { minimum: 2,
     too_short: "%{count} characters are the minimum allowed." }
   validates :event_name, length: { maximum: 255,
