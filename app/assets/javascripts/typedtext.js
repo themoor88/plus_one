7425f3a34f8d4b6955008e02e57fea09589c99b1
Jordan <jordanmcrae@hotmail.com>
2015-06-04 14:05:25 -0400

    Styling

---
+++ app/assets/javascripts/typedtext.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".element").typed({
+    strings: ["play soccer.", "make music.", "talk politics.", "go shopping.", "have coffee.", "play games.", "share experiences.",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000
+  });
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :avatar, :city, :country)
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :avatar, :city, :country, :bio, :interests)
   end
 end
