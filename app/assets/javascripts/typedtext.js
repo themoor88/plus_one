d8633768362fb40f136246add418e7b99bcad64b
Jordan <jordanmcrae@hotmail.com>
2015-06-04 18:45:36 -0400

    Continuing to work on styling and responsiveness.

--- app/assets/javascripts/typedtext.js
+++ app/assets/javascripts/typedtext.js
@@ -6,3 +6,10 @@ $(function(){
     backDelay: 1000
   });
 });
+
+var listItems = document.getElementsByTagName("li");
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].onclick = function() {
+    this.parentNode.removeChild(this);
+  }
+}
