d0d1931b169098e19b222476194c920cf64eeaf2
Jordan <jordanmcrae@hotmail.com>
2015-06-06 09:56:36 -0400

    Fixed notification menu 'click to remove' issue

--- app/assets/javascripts/slidingmenu.js
+++ app/assets/javascripts/slidingmenu.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
     e.preventDefault();
   });
+
+  var listItems = $('.click-to-remove')
+
   for (var i = 0; i < listItems.length; i++) {
     listItems[i].onclick = function() {this.parentNode.removeChild(this);}
   }
