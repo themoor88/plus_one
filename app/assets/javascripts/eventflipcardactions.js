81cf60d6b5ee58e65f0f6a52292266adca66d1d9
Jordan <jordanmcrae@hotmail.com>
2015-06-05 20:19:56 -0400

    Working on flip card animation

---
+++ app/assets/javascripts/eventflipcardactions.js
@@ -0,0 +1,31 @@
+$(function($) {
+  // $("#card").flip();
+  // var fliped = false;
+
+  // $("#card").click(function(){
+  //   if (fliped) {
+  //     $(".front").css({transform: "rotateY(0deg)"})
+  //     $(".back").css({transform: "rotateY(180deg)"})
+  //   } else {
+  //     $(".front").css({transform: "rotateY(180deg)"})
+  //     $(".back").css({transform: "rotateY(0deg)"})
+  //   }
+
+  //   fliped = !fliped;
+  // })
+
+});
+
+////////// RANDOM EVENT BUTTON ///////////
+
+$(function() {
+  var animationClasses = 'animated alternate iteration zoomOut';
+  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+  $('.animate-trigger').on('click',function() {
+    $('.animate-target').addClass(animationClasses).one(animationEnd,function() {
+      $(this).removeClass(animationClasses);
+      $.getScript('/');
+    });
+  });
+});
